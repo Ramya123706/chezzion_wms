05e50dc8d91edb4f3a304cd0e3042fc4f2c9f541
Ramya123706 <ramyanellikumeri@gmail.com>
2025-06-03 15:16:24 +0530

    new

--- chezzsap/rf_dispatch/views.py
+++ chezzsap/rf_dispatch/views.py
@@ -5,3 +5,46 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def fifth(request):
+    return render(request, 'fifth.html')
+
+def sixth(request):
+    return render(request, 'sixth.html')
+
+def first(request):
+    return render(request, 'first.html')
+def second(request):
+    return render(request, 'second.html')
+def hu1(request):
+    return render(request, 'hu1.html')
+def hu12(request):
+    return render(request, 'hu12.html')
+def hu123(request):
+    return render(request, 'hu123.html')
+def new1(request):  
+    return render(request, 'new1.html')
+def new2(request):
+    return render(request, 'new2.html')
+def new3(request):
+    return render(request, 'new3.html')
+def new4(request):
+    return render(request, 'new4.html')
+def one(request):
+    return render(request, 'truck_screen/one.html')
+def two(request):
+    return render(request, 'truck_screen/two.html')
+def three(request):
+    return render(request, 'truck_screen/three.html')
+def four(request):  
+    return render(request, 'truck_screen/four.html')
+def five1(request):
+    return render(request, 'truck_screen/five1.html')
+def six1(request):
+    return render(request, 'truck_screen/six.html')
+def seven1(request):
+    return render(request, 'truck_screen/seven.html')
+def eight1(request):    
+    return render(request, 'truck_screen/eight.html')
+def nine1(request):
+    return render(request, 'truck_screen/nine.html')
