2040ee39d66744565ec0fc910446a6e366d8f736
Ramya123706 <ramyanellikumeri@gmail.com>
2025-06-24 11:21:31 +0530

    new1

--- chezzsap/rf_dispatch/models.py
+++ chezzsap/rf_dispatch/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 YES_NO_CHOICES = [
     ('Yes', 'Yes'),
@@ -35,7 +36,7 @@ class TruckLog(models.Model):
     truck_time = models.TimeField()
     comment = models.TextField()
     status = models.CharField(max_length=20, default='Not planned')
-   
+    status_changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"TruckLog(truck_no={self.truck_no}, truck_date={self.truck_date}, truck_time={self.truck_time})"
